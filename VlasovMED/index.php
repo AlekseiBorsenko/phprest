2d51d5bb0fa7762553f1152500b50b8b86aef7e2
aleksei <aleksei@aleksei-term>
2017-04-01 10:33:01 +0100

    code

---
+++ VlasovMED/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="js/lib/underscore.js"></script>
+        <script src="js/lib/jquery.js"></script>
+        <script src="js/lib/bb.js"></script>
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        <?php
+        echo "something";
+        ?>
+    </body>
+</html>
---
+++ VlasovMED/js/script.js
@@ -0,0 +1,21 @@
+var models = {}
+
+models.Example = Backbone.Model.extend({
+    id: null,
+    name: 'default name'
+})
+
+var ExampleLst = Backbone.Collection.extend({
+    model: models.Example,
+    url: '/VlasovMED/php/lib/crudapi.php/example'
+});
+
+var views = {};
+
+$(function(){
+    var examples = new ExampleLst();
+    examples.fetch({ data: $.param({ name: 1}) });
+    var newEx = new models.Example();
+    examples.add(newEx);
+    newEx.save()
+})
